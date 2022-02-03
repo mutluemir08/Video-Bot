f69ed254ffb44d97b19a9dc5344c96da3c6a159f
aylak-github <85997548+aylak-github@users.noreply.github.com>
2022-02-03 20:13:24 +0300

    Update admins.py

--- program/admins.py
+++ program/admins.py
@@ -24,7 +24,7 @@ bcl = InlineKeyboardMarkup([[InlineKeyboardButton("🗑 Mkapat", callback_data="
 async def update_admin(client, message):
     global admins
     new_admins = []
-    new_ads = await client.get_chat_members(message.chat.id, filter="yönetici listesi")
+    new_ads = await client.get_chat_members(message.chat.id, filter="administrators")
     for u in new_ads:
         new_admins.append(u.user.id)
     admins[message.chat.id] = new_admins
